2f65ba762c9fb6e0d53a83ce49c73d6062ba8ea9
Ugur Emre Dogan <ue.dogan@gmail.com>
2021-03-29 16:51:49 -0700

    Problem 3 Solved.

---
+++ 3-Java_If-Else/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        if (N % 2 == 1) {
+            System.out.println("Weird");
+        } else if ((N % 2 == 0) && (N > 0) && (N < 6)) {
+            System.out.println("Not Weird");
+        } else if ((N % 2 == 0) && (N > 5) && (N < 21)) {
+            System.out.println("Weird");
+        } else if ((N % 2 == 0) && (N > 20)) {
+            System.out.println("Not Weird");
+        }
+    }
+}
